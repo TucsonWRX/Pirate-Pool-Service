6b23f4a8aab907d4a58639e1f5df8f9c98e9a1e6
TucsonWRX <52384059+TucsonWRX@users.noreply.github.com>
2025-05-03 14:38:03 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const testimonials = document.querySelectorAll('.testimonial');
+let index = 0;
+setInterval(() => {
+  testimonials[index].classList.remove('active');
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add('active');
+}, 5000);
